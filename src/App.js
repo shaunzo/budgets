524f5a01710a79a1e0457bb6e89fa2f00df29b65
Shaun Reddiar <shaun.reddiar@absa.africa>
2021-09-07 22:04:41 +0200

    Created Actions functions for Redux

--- src/App.js
+++ src/App.js
@@ -73,8 +73,16 @@ function App() {
       id: 1
     }
 
-    store.dispatch({type: 'ADD_ENTRY', payload: payload_add});
-    store.dispatch({type: 'REMOVE_ENTRY', payload: payload_remove})
+    const addEntryRedux = (payload) => {
+      return {type: 'ADD_ENTRY', payload};
+    }
+
+    const removeEntryRedux = (id) => {
+      return {type: 'REMOVE_ENTRY', payload: {id}}
+    }
+
+    store.dispatch(addEntryRedux(payload_add));
+    store.dispatch(removeEntryRedux(1));
 
   //  
   useEffect(() => {
