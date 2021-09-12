00e8fa88dae3c4fecb57207f87737b1ffe603f63
Shaun Reddiar <shaun.reddiar@absa.africa>
2021-09-12 11:32:19 +0200

    Added Provider to give acces to Redux Store

--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import 'semantic-ui-css/semantic.min.css';
+import { Provider } from 'react-redux';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
