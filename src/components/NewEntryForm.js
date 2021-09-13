26d7e1fe8d52d4db19e208098cca4216ec6395ed
Shaun Reddiar <shaun.reddiar@absa.africa>
2021-09-13 02:10:02 +0200

    Used useDispatch for add entry

--- src/components/NewEntryForm.js
+++ src/components/NewEntryForm.js
@@ -1,15 +1,32 @@
+import { useState } from 'react';
 import { Form } from 'semantic-ui-react';
 import EntryForm from './EntryForm';
 import ButtonSaveOrCancel from './ButtonSaveOrCancel';
+import { useDispatch } from 'react-redux';
+import { addEntryRedux } from '../actions/entries.actions';
 
-function NewEntryForm({
-  addEntry,
-  description,
-  setDescription,
-  value,
-  setValue,
-  isExpense,
-  setIsExpense}) {
+function NewEntryForm() {
+
+  const [description, setDescription] = useState('');
+  const [value, setValue] = useState('');
+  const [isExpense, setIsExpense] = useState(true);
+  const dispatch = useDispatch();
+  
+  const addEntry = () => {
+    dispatch(
+      addEntryRedux({
+        id: 5,
+        description,
+        value,
+        isExpense
+      })
+    );
+
+    setDescription('');
+    setValue('');
+    setIsExpense(true);
+  }
+
     return (
         <Form unstackable>
 
