e7f3202c117279c4e954d68fb1e404d861025af7
Shaun Reddiar <shaun.reddiar@absa.africa>
2021-09-20 12:55:17 +0200

    Query data using Call

--- src/sagas/entriesSaga.js
+++ src/sagas/entriesSaga.js
@@ -1,8 +1,11 @@
-import {take} from 'redux-saga/effects';
+import {call, take} from 'redux-saga/effects';
 import entriesTypes from '../actions/entries.actions';
+import axios from 'axios';
 
 export function* getAllEntries() {
     // Wait till we receive a message to fetch all the entries with take effect
     yield take(entriesTypes.GET_ENTRIES);
     console.log('I need to get the entries now');
+    const result = yield call(axios, 'http://localhost:3001/entries');
+    console.log(result);
 }
