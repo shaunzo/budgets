cff1b34f6386549a1d2c9db0b5bb49fe2e9ef95f
Shaun Reddiar <shaun.reddiar@absa.africa>
2021-09-26 19:15:45 +0200

    Clean up before starting implement Saga Effects

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -1,7 +1,7 @@
-// import * as entriesSaga from '../sagas/entriesSaga';
-import * as testSaga from '../sagas/testSaga';
+import * as entriesSaga from '../sagas/entriesSaga';
+// import * as testSaga from '../sagas/testSaga';
 
 export function initSagas(sagaMiddleware) {
-    Object.values(testSaga).forEach(sagaMiddleware.run.bind(sagaMiddleware));
-    // Object.values(entriesSaga).forEach(sagaMiddleware.run.bind(sagaMiddleware));
+    // Object.values(testSaga).forEach(sagaMiddleware.run.bind(sagaMiddleware));
+    Object.values(entriesSaga).forEach(sagaMiddleware.run.bind(sagaMiddleware));
 }
